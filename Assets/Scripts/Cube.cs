7fbf9e376a270ee18ba87ae720ca1ab6a287008f
VladislavSavich <104223266+VladislavSavich@users.noreply.github.com>
2025-09-12 19:00:05 +0500

    init

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    private Renderer _renderer;
+    private int _minimumValueLifetime = 2;
+    private int _maximumValueLifetime = 6;
+    private Color DefaultColor = Color.white;
+    public BoxCollider Collider { get; private set; }
+    public Rigidbody Rigidbody { get; private set; }
+    public Color Color { get; private set; }
+    public bool IsTouched { get; private set; }
+    public event Action<Cube> CubeFallenDown;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        Collider = GetComponent<BoxCollider>();
+        IsTouched = false;
+    }
+
+    private void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        _renderer.material.color = DefaultColor;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (IsTouched == false) 
+        {
+            if (collision.gameObject.CompareTag("Plane")) 
+            {
+                ChangeColor();
+                StartCoroutine(DetermineLifetime());
+                IsTouched = true;
+            }
+        }
+
+        if (IsTouched != true && collision.gameObject.CompareTag("Plane")) 
+        {
+            IsTouched = true;
+            ChangeColor();
+            StartCoroutine(DetermineLifetime());
+        }
+    }
+
+    public void ChangeColor()
+    {
+        _renderer.material.color = Random.ColorHSV();
+    }
+
+    private IEnumerator DetermineLifetime() 
+    {
+        var wait = new WaitForSeconds(Random.Range(_minimumValueLifetime, _maximumValueLifetime));
+
+        yield return wait;
+
+        CubeFallenDown?.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Cube _cubePrefab;
+
+    private ObjectPool<Cube> _pool;
+    private int _poolCapacity = 5;
+    private int _poolMaxSize = 5;
+    private float _repeatRate = 1f;
+    private Vector3 _maximumSpawnCoordinates = new Vector3(50, 30, 50);
+    private Vector3 _minimumSpawnCoordinates = new Vector3(20, 25, 20);
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<Cube>(
+        createFunc: () => Instantiate(_cubePrefab),
+        actionOnGet: (cube) => ActionOnGet(cube),
+        actionOnRelease: (cube) => cube.gameObject.SetActive(false),
+        actionOnDestroy: (cube) => Destroy(cube),
+        collectionCheck: true,
+        defaultCapacity: _poolCapacity,
+        maxSize: _poolMaxSize);
+    }
+
+    private void ActionOnGet(Cube cube) 
+    {
+        cube.transform.position = GenerateRandomPosition();
+        cube.gameObject.SetActive(true);
+        cube.CubeFallenDown += ReleaseCube;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCubes());
+    }
+
+    private IEnumerator SpawnCubes() 
+    {
+        while (enabled)  
+        {
+            yield return new WaitForSeconds(_repeatRate);
+
+            if (_pool.CountActive < _poolMaxSize)
+            {
+                _pool.Get();
+            }
+        }
+    }
+
+    private void ReleaseCube(Cube cube)
+    {
+        cube.CubeFallenDown -= ReleaseCube;
+        _pool.Release(cube);
+    }
+
+    private Vector3 GenerateRandomPosition() 
+    {
+        float positionX = Random.Range(_minimumSpawnCoordinates.x, _maximumSpawnCoordinates.x);
+        float positionY = Random.Range(_minimumSpawnCoordinates.y, _maximumSpawnCoordinates.y);
+        float positionZ = Random.Range(_minimumSpawnCoordinates.z, _maximumSpawnCoordinates.z);
+
+        return new Vector3(positionX, positionY, positionZ);
+    }
+}
